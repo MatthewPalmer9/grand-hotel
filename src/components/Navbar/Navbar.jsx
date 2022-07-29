243150275493d82937ab0b108cd5bb33707fc25d
Matthew Palmer <matthewpalmer9@outlook.com>
2022-07-29 15:34:39 -0400

    Developed actual navigation menu, with styling animation, and it will appear after implementing the onClick={} event.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 export default function Navbar() {
+  const date = new Date().getFullYear();
+
   return (
     <div className="navbar">
       <div className="sidebar">
@@ -32,9 +34,51 @@ export default function Navbar() {
         </ul>
 
         <div className="year">
-          <p>&copy; 2018</p>
+          <p>{date}</p>
         </div>
       </div>
+
+      <nav className="navigation">
+        <div className="navigation-header">
+          <h1 className="navigation-heading">Grand Hotel</h1>
+
+          <form className="navigation-search">
+            <input 
+              type="text"
+              className="navigation-search-input"
+              placeholder="Search..."
+            />
+            <button className="navigation-search-btn">
+              <i className="fas fa-search" />
+            </button>
+          </form>
+        </div>
+
+        <ul className="navigation-list">
+          <li className="navigation-item">
+            <a href="#" className="navigation-link">Home</a>
+          </li>
+          <li className="navigation-item">
+            <a href="#" className="navigation-link">About Us</a>
+          </li>
+          <li className="navigation-item">
+            <a href="#" className="navigation-link">Rooms</a>
+          </li>
+          <li className="navigation-item">
+            <a href="#" className="navigation-link">Events</a>
+          </li>
+          <li className="navigation-item">
+            <a href="#" className="navigation-link">Customers</a>
+          </li>
+          <li className="navigation-item">
+            <a href="#" className="navigation-link">Contact</a>
+          </li>
+        </ul>
+
+        <div className="copyright">
+          <p>&copy; {date}. Grand Hotel. All rights reserved.</p>
+        </div>
+      </nav>
     </div>
   )
 }
