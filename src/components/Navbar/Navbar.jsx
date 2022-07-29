a92ff5455fdd8c778b7f8bbc020f7a56e0c9c1f9
Matthew Palmer <matthewpalmer9@outlook.com>
2022-07-29 16:15:48 -0400

    Established animation and click event for showing/hiding the navigation menu. Used CSS instead of JavaScript for the click event.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,17 @@ export default function Navbar() {
 
   return (
     <div className="navbar">
+
+      <input type="checkbox" className="checkbox" id="click" hidden />
+
       <div className="sidebar">
-        <div className="menu-icon">
-          <div className="line line-1"></div>
-          <div className="line line-2"></div>
-          <div className="line line-3"></div>
-        </div>
+        <label for="click">
+          <div className="menu-icon">
+            <div className="line line-1"></div>
+            <div className="line line-2"></div>
+            <div className="line line-3"></div>
+          </div>
+        </label>
 
         <ul className="social-icons-list">
           <li>
